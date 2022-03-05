6d5dc7f7fe73f27cddd2836ca5905fa85b812a4b
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-03-05 13:56:38 +0000

    Diagnostics get route

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,13 +5,14 @@ const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 // GET Route for retrieving diagnostic information
 diagnostics.get("/", (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
-  writeToFile("./db/diagnostics.json", res);
+  readFromFile("./db/diagnostics.json").then((data) =>
+    res.json(JSON.parse(data))
+  );
 });
 
 // POST Route for a error logging
 diagnostics.post("/", (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
-  readAndAppend(req, "./db/diagnostics.json");
 });
 
 module.exports = diagnostics;
